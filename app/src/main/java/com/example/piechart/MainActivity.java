d44eaec166e3bc4b32f6870e064fd49a8b3d53a0
aki0207 <forever.look.aki@docome.ne.jp>
2019-01-26 17:28:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/piechart/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.piechart;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sample_pie_chart_view);
+
+        PieChartView pieChartView = (PieChartView) findViewById(R.id.pie_chart);
+
+        List<PieChartView.PieceDataHolder> pieceDataHolders = new ArrayList<>();
+
+        pieceDataHolders.add(new PieChartView.PieceDataHolder(2,0xFF77CCAA, "今天，１"));
+        pieceDataHolders.add(new PieChartView.PieceDataHolder(5, 0xFF11AA33, "明天，２"));
+        pieceDataHolders.add(new PieChartView.PieceDataHolder(3, Color.GRAY, "就是风，３"));
+      /*  pieceDataHolders.add(new PieChartView.PieceDataHolder(5000, Color.YELLOW, "呵呵，４"));
+        pieceDataHolders.add(new PieChartView.PieceDataHolder(10000, Color.RED, "小京，５"));
+        pieceDataHolders.add(new PieChartView.PieceDataHolder(13000, Color.BLUE, "花花，６"));*/
+
+        pieChartView.setData(pieceDataHolders);
+
+    }
+}
